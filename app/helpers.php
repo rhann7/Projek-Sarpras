6b3dfc2319e6578cd657a32345049ec60b47ab0a
rhann7 <raihandityam09@gmail.com>
2025-05-15 20:17:39 +0700

    Create: Logs helper

---
+++ app/helpers.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\Log;
+use Illuminate\Support\Facades\Auth;
+
+if (!function_exists('logActivity')) {
+    function logActivity(string $action, string $target, int $targetId, ?string $description = null): void
+    {
+        Log::create([
+            'user_id'     => Auth::id(),
+            'action'      => $action,
+            'target'      => $target,
+            'target_id'   => $targetId,
+            'description' => $description,
+        ]);
+    }
+}
